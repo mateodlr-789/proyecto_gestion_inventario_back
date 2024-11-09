a4f6c3994968cb9e3fe5438585f37b1c7e163dc8
MrRichar03 <richarmadina03@gmail.com>
2024-11-09 14:57:34 -0500

    fix(routes): i fix the yup schemas path in the imports so that the routes can use them

--- routes/order-product.ts
+++ routes/order-product.ts
@@ -2,7 +2,9 @@ import { Router } from 'express';
 
 import { validateSchema } from '../middlewares/validate-schema';
 import { errorHandler } from '../middlewares/error-handler';
-import { addOrderProduct, addOrderProductSchema, getOrderProduct, idSchema } from '../controllers/order-product';
+import { addOrderProduct, getOrderProduct } from '../controllers/order-product';
+import { idSchema } from '../request/shared';
+import { addOrderProductSchema } from '../request/order-product';
 
 const router = Router();
 
