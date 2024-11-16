dd56ac4c47fe6aafe6ead2f0559db3ccd61b0986
MrRichar03 <richarmadina03@gmail.com>
2024-11-16 13:13:32 -0500

    feat(routes): update stock route
    i added a route for the function that updates the stock of a product

--- routes/product.ts
+++ routes/product.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { errorHandler } from '../middlewares/error-handler';
 
-import { createProduct,  deleteProduct, getProducts } from '../controllers/product';
+import { createProduct, deleteProduct, getProducts, updateStock } from '../controllers/product';
 import { validateSchema } from '../middlewares/validate-schema';
 import { createProductSchema } from '../request/product';
 import { idSchema } from '../request/shared';
 
 const router = Router();
 
-router.get('/',   getProducts);
+router.get('/', getProducts);
 
 router.post(
     '/',
@@ -21,6 +21,12 @@ router.delete(
     validateSchema(idSchema, 'params'),
     deleteProduct
 );
+
+router.post(
+    '/:id/stock',
+    validateSchema(idSchema, 'params'),
+    updateStock
+)
 
 router.use(errorHandler);
 
